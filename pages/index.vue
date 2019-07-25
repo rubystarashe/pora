20cf6026ee11710ddf6a985ccc571064a29ef64d
rubystarashe <rubystarashe@gmail.com>
2019-07-26 05:44:48 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,9 @@
       <div class="title_main_description">패스 오브 엑자일 유저 애드온</div>
     </div>
   </div>
-  <div class="download_area" @click="download()">
+  <div class="download_area">
     <div class="download_descriptions">
-      <div class="download_description">현재 버전: 1.1.1</div>
+      <div class="download_description">현재 버전: 1.1.2</div>
       <div class="download_description">- 아이템에 Ctrl+C 키를 눌러 시세를 즉시 확인할 수 있어요</div>
       <div class="download_description">- 거래 메시지가 오면 애드온으로 알려줘요</div>
       <div class="download_description">- 경매장에서 거래 메시지를 복사하면 손쉽게 붙여넣을 수 있어요.</div>
@@ -17,28 +17,17 @@
       <div class="download_description">- 창고의 가치를 한눈에 파악할 수 있어요</div>
       <div class="download_description">- 비싼 아이템을 획득할시 자동으로 알려줘요</div>
       <div class="download_description">- Ctrl + 마우스 휠 로 손쉽게 창고를 선택할 수 있어요</div>
-      <div class="download_description">- F3 키를 통해 검색 오버레이를 이용할 수 있어요(현재 커런시 검색 기능만 가능)</div>
+      <div class="download_description">- 여러가지 검색 오버레이를 이용할 수 있어요(현재 커런시 검색 기능만 가능)</div>
       <div class="download_description">이 애드온은 가계부 기능을 위해 유저의 총 카오스 오브 개수에 대한 정보를 수집합니다.</div>
       <div class="download_description">이 애드온은 거래 애드온을 위해 유저의 계정명과 캐릭터명, 최근 이용한 캐릭터명을 수집합니다.</div>
-      <div class="download_description">이 애드온은 서버 성능과 유지에 대한 문제로, 유저의 업로드 자원을 일부 사용할 수 있습니다.</div>
       <div class="download_description">이 애드온은 순수 독자적 커뮤니케이션 기능을 제외한 정보 이용으로는 수익창출 하지 않습니다.</div>
       <div class="download_description">이 애드온을 다운로드 하여 사용시 위의 모든 사항에 동의하는것으로 간주합니다.</div>
     </div>
+    <a href="https://firebasestorage.googleapis.com/v0/b/spinel-2019.appspot.com/o/Path%20of%20RubystarAshe%20Setup%201.1.2.exe?alt=media&token=dde94203-b57c-4a4d-a9f4-e6797bb9f538">
     <div class="download_btn_area">
-      <div v-if="!onstart">1.1.1 다운로드</div>
-      <div v-else-if="done">다운로드 완료!</div>
-      <div v-else>
-        <div v-if="!progress">다운로드를 준비중이에요</div>
-        <div class="progress_bg" :style="{ width: Math.floor(progress * 100) + '%' }"/>
-        <div v-if="progress" class="progress">{{Math.floor(progress * 100)}}% {{bytesToSize(downloadSpeed || 0)}}/s</div>
-      </div>
+      <div>1.1.2 다운로드</div>
     </div>
-  </div>
-  <div class="help_area" v-if="help && !progress && !done">
-    <div class="help_box">
-      <div>다운로드가 진행이 안되시면 아래 링크로 다운로드 받아보세요!</div>
-      <a href="https://firebasestorage.googleapis.com/v0/b/spinel-2019.appspot.com/o/Path%20of%20RubystarAshe%20Setup%201.1.1.exe?alt=media&token=32ac5319-27e7-4494-b32f-8319da851c68">서버를 통한 다운로드</a>
-    </div>
+    </a>
   </div>
 </div>
 </template>
